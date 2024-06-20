6a2fc766dc27d4689d4a3a61c6e0dcfc1bdac526
Paul Michael <paullmich28@gmail.com>
2024-06-21 06:58:36 +0700

    Initial Commit

---
+++ GitBranchTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GitBranchTest
+//
+//  Created by Paulus Michael on 21/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GitBranchTest/GitBranchTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitBranchTestApp.swift
+//  GitBranchTest
+//
+//  Created by Paulus Michael on 21/06/24.
+//
+
+import SwiftUI
+
+@main
+struct GitBranchTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
